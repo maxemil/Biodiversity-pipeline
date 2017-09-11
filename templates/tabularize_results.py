3f6c2d8c59c2ce932715316aa656177a0c1acb3b
maxemil <max-emil.schon@icm.uu.se>
2017-09-11 09:36:30 +0200

    Add script to summarize results from different processes

---
+++ templates/tabularize_results.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+from ete3 import NCBITaxa
+
+
+ncbi = NCBITaxa()
+
+funguild = pd.read_csv("$funguild", sep='\\t', index_col="SequenceID")
+motu_info = pd.read_csv("$motu_info", sep='\\t', names=["MOTU", "SequenceID"], index_col="SequenceID")
+sequence_species = pd.read_csv("$sequence_species", sep='\\t', index_col="SequenceID")
+
+sequence_species['species'] = sequence_species['taxonomy'].apply(lambda x: ncbi.get_taxid_translator([x])[x])
+
+mergedf = pd.merge(sequence_species, funguild[['Guild', 'Trophic Mode']],
+                            left_index=True, right_index=True)
+mergedf = pd.merge(motu_info, mergedf, left_index=True, right_index=True)
+mergedf.to_csv("${sequence_species.baseName}.tab", sep='\\t')
